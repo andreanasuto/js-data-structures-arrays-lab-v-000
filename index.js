cc2a57046b0d5b62af796a8342aa6128bcf09f11
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-27 18:24:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const drivers = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendDriver(name) {
   return drivers.push(name)
 }
+
+function destructivelyPrependDriver(name) {
+  return drivers.slice(2)
+}
+
+function destructivelyRemoveLastDriver() {
+  return drivers.unshift
+}
+
+fu
